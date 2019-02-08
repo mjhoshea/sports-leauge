87b14714ddf42d5c91b83be44f595366b67b0fd9
Michael Oshea <9mjho9@gmail.com>
2019-02-08 22:37:04 +0000

    feat: add dashboard template

---
+++ src/components/league/LeagueTable.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const LeagueTable = () => {
+    return (
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card content grey-text text-darken-3"> 
+                    <span className="card-title">LeagueTable</span>
+                    <p>a</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card content grey-text text-darken-3"> 
+                    <span className="card-title">LeagueTable</span>
+                    <p>a</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card content grey-text text-darken-3"> 
+                    <span className="card-title">LeagueTable</span>
+                    <p>a</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default  LeagueTable
