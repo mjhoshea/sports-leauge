7595b70bb38e7f52cc2b9530238de516b6ad9a87
Michael Oshea <9mjho9@gmail.com>
2019-02-10 18:42:54 +0000

    feat: DOM render wait for firebase auth

--- src/index.js
+++ src/index.js
@@ -11,17 +11,16 @@ import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig'
 
+
 const store = createStore(rootReducer,
-    compose(
-      applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-      reactReduxFirebase(fbConfig), // redux binding for firebase
-      reduxFirestore(fbConfig) // redux bindings for firestore
-    )
-  );
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbConfig, {attachAuthIsReady: true}), // redux binding for firebase
+    reduxFirestore(fbConfig) // redux bindings for firestore
+  )
+);
 
-ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+  serviceWorker.unregister();
+});
